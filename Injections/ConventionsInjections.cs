cab074d63dfca0c59a4f7e9118817a78cd7cac19
Alexandre JOURNET <alexandre.journet@gmail.com>
2024-10-21 22:44:51 +0200

    wip

--- Injections/ConventionsInjections.cs
+++ Injections/ConventionsInjections.cs
@@ -3,24 +3,24 @@ using Solstice.Database.Conventions.NamingConventions;
 
 namespace Solstice.Database.Conventions.Injections;
 
-public class ConventionsInjections
+public static class ConventionsInjections
 {
-    public static void ToSnakeCase(ModelConfigurationBuilder configurationBuilder)
+    public static void ToSnakeCase(this ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Conventions.Add(_ => new SnakeCaseNamingConvention());
     }
     
-    public static void ToUpperCase(ModelConfigurationBuilder configurationBuilder)
+    public static void ToUpperCase(this ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Conventions.Add(_ => new UpperCaseNamingConvention());
     }
     
-    public static void ToLowerCase(ModelConfigurationBuilder configurationBuilder)
+    public static void ToLowerCase(this ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Conventions.Add(_ => new LowerCaseNamingConvention());
     }
     
-    public static void ToUpperSnakeCase(ModelConfigurationBuilder configurationBuilder)
+    public static void ToUpperSnakeCase(this ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Conventions.Add(_ => new UpperSnakeCaseNamingConvention());
     }
